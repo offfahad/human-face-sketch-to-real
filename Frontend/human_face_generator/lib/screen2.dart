27fc6498d579e69d03e2559988dd223f118df579
offfahad <mughalfahad544@gmail.com>
2024-02-20 18:43:49 +0500

    sketch-upload-done

--- Frontend/human_face_generator/lib/screen2.dart
+++ Frontend/human_face_generator/lib/screen2.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:human_face_generator/custom_painter.dart';
 import 'package:human_face_generator/drawing_point.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Screen2 extends StatefulWidget {
   const Screen2({super.key});
@@ -38,10 +39,13 @@ class _Screen2State extends State<Screen2> {
   Widget? imageOutput = Container();
 
   bool isSavedPressed = false;
-  var sketchSaveBytes;
   Uint8List? convertedBytes;
-
+  var listBytes;
   final GlobalKey imageKey = GlobalKey();
+
+  final ImagePicker picker = ImagePicker();
+  File? UserPickedImage;
+  List? output;
 
   void saveToImage(List<DrawingPoint?> points) async {
     final recorder = ui.PictureRecorder();
@@ -71,46 +75,15 @@ class _Screen2State extends State<Screen2> {
     final picture = recorder.endRecording();
     final img = await picture.toImage(256, 256);
     final pngBytes = await img.toByteData(format: ui.ImageByteFormat.png);
-    final listBytes = Uint8List.view(pngBytes!.buffer);
+    listBytes = Uint8List.view(pngBytes!.buffer);
     //File file = await writeBytes(listBytes);
     String base64 = base64Encode(listBytes);
     fetchResponse(base64);
   }
 
-  void saveToImageGallary(List<DrawingPoint?> points) async {
-    final recorder = ui.PictureRecorder();
-    final canvas = Canvas(
-      recorder,
-      Rect.fromPoints(const Offset(0.0, 0.0), const Offset(256, 256)),
-    );
-    Paint paint = Paint()
-      ..color = const ui.Color.fromARGB(255, 0, 0, 0)
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = 2.0;
-    final paint2 = Paint()
-      ..style = PaintingStyle.fill
-      ..color = const ui.Color.fromARGB(255, 255, 255, 255);
-    canvas.drawRect(const Rect.fromLTWH(0, 0, 256, 256), paint2);
-    for (int i = 0; i < points.length - 1; i++) {
-      if (points[i] != null && points[i + 1] != null) {
-        for (int j = 0; j < points[i]!.offsets.length - 1; j++) {
-          canvas.drawLine(
-            points[i]!.offsets[j],
-            points[i]!.offsets[j + 1],
-            paint,
-          );
-        }
-      }
-    }
-    final picture = recorder.endRecording();
-    final img = await picture.toImage(256, 256);
-    final pngBytes = await img.toByteData(format: ui.ImageByteFormat.png);
-    sketchSaveBytes = Uint8List.view(pngBytes!.buffer);
-  }
-
   void fetchResponse(var base64Image) async {
     var data = {"Image": base64Image};
-    var url = Uri.parse("http://192.168.43.199:5000/predict");
+    var url = Uri.parse("http://192.168.249.56:5000/predict");
 
     Map<String, String> headers = {
       'Content-type': 'application/json',
@@ -176,7 +149,7 @@ class _Screen2State extends State<Screen2> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('No Sketch Found'),
+          title: const Text('No Image Found'),
           content: const Text('Draw A Sketch First.'),
           actions: [
             TextButton(
@@ -189,6 +162,87 @@ class _Screen2State extends State<Screen2> {
         );
       },
     );
+  }
+
+  void saveRealImageToGallery() async {
+    if (imageOutput != null) {
+      RenderRepaintBoundary boundary =
+          imageKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+      ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+      ByteData? byteData =
+          await image.toByteData(format: ui.ImageByteFormat.png);
+      Uint8List pngBytes = byteData!.buffer.asUint8List();
+
+      // Save the image to the device's gallery
+      final result =
+          await ImageGallerySaver.saveImage(Uint8List.fromList(pngBytes));
+      // Check if the image was saved successfully
+      if (result != null) {
+        showImageSavedDialog(context);
+      } else {
+        showImageNotSavedDialog(context);
+      }
+    } else {
+      showImageNotSavedDialog(context);
+    }
+  }
+
+  Future<ui.Image?> decodeImageFromFile(File imageFile) async {
+    final Uint8List bytes = await imageFile.readAsBytes();
+    final Completer<ui.Image> completer = Completer();
+    ui.decodeImageFromList(bytes, (ui.Image img) {
+      completer.complete(img);
+    });
+    return completer.future;
+  }
+
+  Future<void> loadImage(XFile? file) async {
+    if (file != null) {
+      final File pickedImage = File(file.path);
+      final decodedImage = await decodeImageFromFile(pickedImage);
+      if (decodedImage != null &&
+          decodedImage.width == 256 &&
+          decodedImage.height == 256) {
+        setState(() {
+          UserPickedImage = pickedImage;
+        });
+        var base64String = await fileToBase64(file);
+        fetchResponse(base64String);
+      } else {
+        // Show a dialog box indicating the image is not supported
+        // ignore: use_build_context_synchronously
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Image Not Supported'),
+              content: const Text(
+                  'Please choose a sketch image which you saved from this application.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    }
+  }
+
+  Future<String> fileToBase64(XFile file) async {
+    List<int> bytes = await file.readAsBytes();
+    return base64Encode(bytes);
+  }
+
+  Future<void> pickImage() async {
+    final XFile? file = await picker.pickImage(source: ImageSource.gallery);
+    if (file != null) {
+      loadImage(file);
+    }
   }
 
   @override
@@ -205,8 +259,8 @@ class _Screen2State extends State<Screen2> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                      width: 256,
-                      height: 256,
+                      width: 257,
+                      height: 257,
                       decoration: BoxDecoration(
                         borderRadius: const BorderRadius.all(
                           Radius.circular(0),
@@ -218,63 +272,64 @@ class _Screen2State extends State<Screen2> {
                           width: 1, // Set the border width here
                         ),
                       ),
-                      child: GestureDetector(
-                        onPanStart: (details) {
-                          setState(() {
-                            currentDrawingPoint = DrawingPoint(
-                              id: DateTime.now().microsecondsSinceEpoch,
-                              offsets: [
-                                details.localPosition,
-                              ],
-                              color: selectedColor,
-                              width: selectedWidth,
-                            );
-                            if (currentDrawingPoint == null) return;
-                            drawingPoints.add(currentDrawingPoint!);
-                            historyDrawingPoints = List.of(drawingPoints);
-                          });
-                        },
-                        onPanUpdate: (details) {
-                          setState(() {
-                            if (currentDrawingPoint == null) return;
-                            currentDrawingPoint = currentDrawingPoint?.copyWith(
-                              offsets: currentDrawingPoint!.offsets
-                                ..add(details.localPosition),
-                            );
-                            drawingPoints.last = currentDrawingPoint!;
-                            historyDrawingPoints = List.of(drawingPoints);
-                          });
-                        },
-                        onPanEnd: (details) {
-                          saveToImage(drawingPoints);
-                          saveToImageGallary(drawingPoints);
-                          currentDrawingPoint = null;
-                        },
-                        child: CustomPaint(
-                          painter: DrawingPainter(
-                            drawingPoints: drawingPoints,
-                          ),
-                          size: const Size(256, 256),
-                        ),
-                      ),
+                      child: UserPickedImage != null
+                          ? Image.file(UserPickedImage!)
+                          : GestureDetector(
+                              onPanStart: (details) {
+                                setState(() {
+                                  currentDrawingPoint = DrawingPoint(
+                                    id: DateTime.now().microsecondsSinceEpoch,
+                                    offsets: [
+                                      details.localPosition,
+                                    ],
+                                    color: selectedColor,
+                                    width: selectedWidth,
+                                  );
+                                  if (currentDrawingPoint == null) return;
+                                  drawingPoints.add(currentDrawingPoint!);
+                                  historyDrawingPoints = List.of(drawingPoints);
+                                });
+                              },
+                              onPanUpdate: (details) {
+                                setState(() {
+                                  if (currentDrawingPoint == null) return;
+                                  currentDrawingPoint =
+                                      currentDrawingPoint?.copyWith(
+                                    offsets: currentDrawingPoint!.offsets
+                                      ..add(details.localPosition),
+                                  );
+                                  drawingPoints.last = currentDrawingPoint!;
+                                  historyDrawingPoints = List.of(drawingPoints);
+                                });
+                              },
+                              onPanEnd: (details) {
+                                saveToImage(drawingPoints);
+                                currentDrawingPoint = null;
+                              },
+                              child: CustomPaint(
+                                painter: DrawingPainter(
+                                  drawingPoints: drawingPoints,
+                                ),
+                                size: const Size(256, 256),
+                              ),
+                            ),
                     ),
                     const SizedBox(height: 50),
                     Container(
-                      width: 256,
-                      height: 256,
+                      width: 257,
+                      height: 257,
                       decoration: BoxDecoration(
                         border: Border.all(color: Colors.black, width: 1.0),
                       ),
-                      child: imageOutput != null
-                          ? imageOutput
-                          : Center(
+                      child: imageOutput ??
+                          const Center(
                               child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text("No AI generated image yet!"),
-                                Text("Draw a face sketch first!")
-                              ],
-                            )),
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("No AI generated image yet!"),
+                              Text("Draw a face sketch first!")
+                            ],
+                          )),
                     ),
                   ],
                 ),
@@ -334,7 +389,9 @@ class _Screen2State extends State<Screen2> {
                             onPressed: () {
                               drawingPoints.clear();
                               historyDrawingPoints.clear();
-                              //image = null;
+                              setState(() {
+                                UserPickedImage = null;
+                              });
                             },
                             icon: const Icon(
                               Icons.delete,
@@ -349,7 +406,7 @@ class _Screen2State extends State<Screen2> {
                               setState(() {
                                 if (drawingPoints.isNotEmpty) {
                                   final result = ImageGallerySaver.saveImage(
-                                      Uint8List.fromList(sketchSaveBytes));
+                                      Uint8List.fromList(listBytes));
                                   if (result != null) {
                                     showImageSavedDialog(context);
                                   } else {
@@ -370,7 +427,7 @@ class _Screen2State extends State<Screen2> {
                           ),
                           IconButton(
                             onPressed: () {
-                              //chooseImage();
+                              pickImage();
                             },
                             icon: const Icon(
                               Icons.file_upload_outlined,
@@ -408,33 +465,7 @@ class _Screen2State extends State<Screen2> {
                             height: 0,
                           ),
                           IconButton(
-                            onPressed: () async {
-                              if (imageOutput != null) {
-                                RenderRepaintBoundary boundary =
-                                    imageKey.currentContext!.findRenderObject()
-                                        as RenderRepaintBoundary;
-                                ui.Image image =
-                                    await boundary.toImage(pixelRatio: 3.0);
-                                ByteData? byteData = await image.toByteData(
-                                    format: ui.ImageByteFormat.png);
-                                Uint8List pngBytes =
-                                    byteData!.buffer.asUint8List();
-
-                                // Save the image to the device's gallery
-                                final result =
-                                    await ImageGallerySaver.saveImage(
-                                        Uint8List.fromList(pngBytes));
-
-                                // Check if the image was saved successfully
-                                if (result != null) {
-                                  showImageSavedDialog(context);
-                                } else {
-                                  showImageNotSavedDialog(context);
-                                }
-                              } else {
-                                showImageNotSavedDialog(context);
-                              }
-                            },
+                            onPressed: saveRealImageToGallery,
                             icon: const Icon(
                               Icons.save,
                               color: Colors.white,

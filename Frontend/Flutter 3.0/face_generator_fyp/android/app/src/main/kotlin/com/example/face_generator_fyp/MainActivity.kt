efe1a1c2d0bee2e6ed805d57a8d236984fed9865
Muhammad Fahad <mughalfahad544@gmail.com>
2024-06-28 19:04:06 +0500

    Delete Frontend/Flutter 3.0/face_generator_fyp directory

--- Frontend/Flutter 3.0/face_generator_fyp/android/app/src/main/kotlin/com/example/face_generator_fyp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.face_generator_fyp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Frontend/Flutter 3.0/face_generator_fyp/lib/models/drawingArea.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class DrawingArea {
-  Offset? point;
-  Paint areaPaint;
-
-  DrawingArea({this.point, required this.areaPaint});
-}
-

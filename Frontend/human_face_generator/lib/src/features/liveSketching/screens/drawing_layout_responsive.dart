85f35ab74f67f669213ebf3d7098b7ccdb5178fd
offfahad <mughalfahad544@gmail.com>
2025-04-22 13:07:11 +0500

    files changes

--- Frontend/human_face_generator/lib/src/features/liveSketching/screens/drawing_layout_responsive.dart
+++ Frontend/human_face_generator/lib/src/features/liveSketching/screens/drawing_layout_responsive.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:human_face_generator/src/features/btm_bar.dart';
 import 'package:human_face_generator/src/features/liveSketching/screens/drawing_screen_desktop.dart';
 import 'package:human_face_generator/src/features/liveSketching/screens/drawing_screen_mobile.dart';
 import 'package:human_face_generator/src/features/responsive_layout.dart';
@@ -9,12 +7,14 @@ class DrawingResponsiveLayout extends StatefulWidget {
   const DrawingResponsiveLayout({super.key});
 
   @override
-  State<DrawingResponsiveLayout> createState() => _DrawingResponsiveLayoutState();
+  State<DrawingResponsiveLayout> createState() =>
+      _DrawingResponsiveLayoutState();
 }
 
 class _DrawingResponsiveLayoutState extends State<DrawingResponsiveLayout> {
   @override
   Widget build(BuildContext context) {
-    return const ResponsiveLayout(mobileBody: DrawingScreen(), desktopBody: DrawingScreenDesktop());
+    return const ResponsiveLayout(
+        mobileBody: DrawingScreen(), desktopBody: DrawingScreenDesktop());
   }
 }
